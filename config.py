5be559167524b124864790155880142f0ecb33e6
WhCollar <monserka@gmail.com>
2022-03-25 16:07:03 +0500

    Save

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ default_mask = {
 }
 
 all_mask = {
-    9: {"decimal notation": "255.128.0.0", "subnets": '', "addresses": "8388606", "class": "128A"},
-    10: {"decimal notation": "255.192.0.0", "subnets": '', "addresses": "4194302", "class": "64A"},
-    11: {"decimal notation": "255.224.0.0", "subnets": '', "addresses": "2097150", "class": "32A"},
-    12: {"decimal notation": "255.240.0.0", "subnets": '', "addresses": "1048574", "class": "16A"},
-    13: {"decimal notation": "255.248.0.0", "subnets": '', "addresses": "524286", "class": "8A"},
-    14: {"decimal notation": "255.252.0.0", "subnets": '', "addresses": "262142", "class": "4A"},
-    15: {"decimal notation": "255.254.0.0", "subnets": '', "addresses": "131070", "class": "2A"},
-    16: {"decimal notation": "255.255.0.0", "subnets": '', "addresses": "65534", "class": "1B"},
+    9: {"decimal notation": "255.128.0.0", "subnets": '2', "addresses": "8388606", "class": "128A"},
+    10: {"decimal notation": "255.192.0.0", "subnets": '4', "addresses": "4194302", "class": "64A"},
+    11: {"decimal notation": "255.224.0.0", "subnets": '8', "addresses": "2097150", "class": "32A"},
+    12: {"decimal notation": "255.240.0.0", "subnets": '16', "addresses": "1048574", "class": "16A"},
+    13: {"decimal notation": "255.248.0.0", "subnets": '32', "addresses": "524286", "class": "8A"},
+    14: {"decimal notation": "255.252.0.0", "subnets": '64', "addresses": "262142", "class": "4A"},
+    15: {"decimal notation": "255.254.0.0", "subnets": '128', "addresses": "131070", "class": "2A"},
+    16: {"decimal notation": "255.255.0.0", "subnets": '256', "addresses": "65534", "class": "1B"},
     17: {"decimal notation": "255.255.128.0", "subnets": '2', "addresses": "32766", "class": "128B"},
     18: {"decimal notation": "255.255.192.0", "subnets": '4', "addresses": "16382", "class": "64B"},
     19: {"decimal notation": "255.255.224.0", "subnets": '8', "addresses": "8190", "class": "32B"},
